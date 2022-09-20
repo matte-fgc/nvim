58137d483d1276962ea3bbe5fd36189b8dfc07e8
matte-fgc <114000820+matte-fgc@users.noreply.github.com>
2022-09-20 17:02:07 +0200

    Update init.lua

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,1 +1,22 @@
+return require("packer").startup(function() 
 
+    use "wbthomason/packer.nvim"
+    use "navarasu/onedark.nvim"
+    use "kyazdani42/nvim-tree.lua"
+    use "akinsho/toggleterm.nvim"
+    use "nvim-lualine/lualine.nvim"
+    use "romgrk/barbar.nvim"
+    use "kyazdani42/nvim-web-devicons"
+    use "nvim-telescope/telescope.nvim"
+    use "nvim-lua/plenary.nvim"
+    use "windwp/nvim-autopairs"
+    use "ellisonleao/gruvbox.nvim"
+
+    use "neovim/nvim-lspconfig"
+    use "hrsh7th/nvim-cmp" 
+    use "hrsh7th/cmp-nvim-lsp" 
+    use "saadparwaiz1/cmp_luasnip" 
+    use "L3MON4D3/LuaSnip"
+    use "onsails/lspkind.nvim"
+
+end)
