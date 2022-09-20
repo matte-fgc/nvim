d4c463f16c62019891298246f6bfaff3d43a9737
matte-fgc <114000820+matte-fgc@users.noreply.github.com>
2022-09-20 17:01:26 +0200

    Update init.lua

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,1 +1,16 @@
+local map = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
+
+vim.g.mapleader = " "
+
+map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+map("n", "<leader>t", ":ToggleTerm<CR>", opts)
+map("n", "<leader>b", ":BufferNext<CR>", opts)
+map("n", "<leader>s", ":vsplit<CR>", opts)
+
+map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+map("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+
 
