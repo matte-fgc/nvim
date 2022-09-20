87cb713b4bc47b7c03d55eb1e6a5151f30c5dde6
matte-fgc <114000820+matte-fgc@users.noreply.github.com>
2022-09-20 17:03:06 +0200

    Update init.lua

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -1,1 +1,10 @@
+require("toggleterm").setup {
 
+    hide_numbers = true,
+    shade_terminals = false,
+    start_in_insert = true,
+    insert_mappings = true,
+    terminal_mappings = true,
+    close_on_exit = true,
+
+}
